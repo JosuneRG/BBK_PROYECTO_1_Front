ff997135013cbb5f688348d5b2fd0c006814ffdd
JosuneRG <josunerg94@gmail.com>
2025-07-14 20:21:40 +0200

    Cambios en approuter

--- Front_E-commerce/src/routes/AppRouter.jsx
+++ Front_E-commerce/src/routes/AppRouter.jsx
@@ -18,20 +18,6 @@ const AppRouter = () => {
     </Routes>
   )
 }
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from '../pages/Login.jsx';
-// otros imports...
 
-function AppRouter() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        {/* otras rutas */}
-      </Routes>
-    </Router>
-  );
-}
 
 export default AppRouter;
