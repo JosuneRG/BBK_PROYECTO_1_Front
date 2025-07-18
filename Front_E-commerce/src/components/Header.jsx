7acb7f4551972a07c43b9d5b8a322f5a08f5b333
cesytar-web <cesytarr@gmail.com>
2025-07-18 17:55:47 +0200

    logica de carrito añadida

--- Front_E-commerce/src/components/Header.jsx
+++ Front_E-commerce/src/components/Header.jsx
@@ -1,3 +1,4 @@
+/*
 import { Link } from 'react-router-dom';
 
 import { NavLink, useNavigate } from 'react-router-dom'
@@ -76,6 +77,58 @@ function Header() {
       </nav>
     </header>
   )
+}
+
+export default Header;
+*/
+
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { CartContext } from "../context/CartContext";
+import "../styles/Header.scss";
+
+function Header() {
+  const { user, logout } = useContext(AuthContext);
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="nav-left">
+          <NavLink to="/" className="home-link">
+            📚 Librería BBK
+          </NavLink>
+        </div>
+
+        <div className="nav-center">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/cart">
+            Carrito ({cartItems.length})
+          </NavLink>
+        </div>
+
+        <div className="nav-right">
+          {user ? (
+            <>
+              <NavLink to="/profile">{user.name || "Perfil"}</NavLink>
+              <button onClick={logout} style={{marginLeft: "1rem", cursor: "pointer"}}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/register">Register</NavLink>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
