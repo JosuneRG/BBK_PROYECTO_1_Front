029aeeb5be380031e5b80e52304259c983fdc72d
cesytar-web <cesytarr@gmail.com>
2025-07-19 09:49:18 +0200

    Modificaciones del Login

--- Front_E-commerce/src/App.jsx
+++ Front_E-commerce/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 import Layout from "./layout/MainLayout";
 import Cart from "./pages/Cart";
 
@@ -13,6 +14,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="profile" element={<Profile />} />
         <Route path="cart" element={<Cart />} />
       </Route>
     </Routes>
--- Front_E-commerce/src/pages/Login.jsx
+++ Front_E-commerce/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importamos para redireccionar
+import { useNavigate, Link } from 'react-router-dom'; // Asegúrate de importar Link
+import "../styles/Login.scss";
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Hook para redirigir
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +19,7 @@ function Login() {
     if (email === 'test@test.com' && password === 'test123') {
       setError('');
       alert('¡Login exitoso!');
-      navigate('/'); // Redirige a Home u otra ruta que quieras
+      navigate('/profile');
     } else {
       setError("Email o contraseña incorrectos");
     }
@@ -45,10 +46,17 @@ function Login() {
         />
 
         <button type="submit">Entrar</button>
+
+        {/* Mensaje de error si existe */}
+        {error && <p className="error">{error}</p>}
+
+        {/* Enlace de registro */}
+        <p className="register-link">
+          ¿No tienes cuenta? <Link to="/register">Regístrate</Link>
+        </p>
       </form>
     </div>
-  )
+  );
 }
 
 export default Login;
-
